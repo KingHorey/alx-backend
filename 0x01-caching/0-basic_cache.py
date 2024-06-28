14b4d096a05544b3ec9e23a16adee6417cbb5404
kingHorey <Oreoluwaakinbo.oa@gmail.com>
2024-06-28 15:48:41 +0100

    change conditional if both are null

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -11,7 +11,7 @@ class BasicCache(BaseCaching):
     """ implementation of a prototype cache """
     def put(self, key: str, item: Union[str, int]) -> None:
         """ add item to the cache """
-        if key is not None or item is not None:
+        if key is not None and item is not None:
             self.cache_data.update({key: item})
 
     def get(self, key: str) -> Union[str, None]:
