e940513777b8d5a70a9b46dcfec7ec5958795ee8
kingHorey <Oreoluwaakinbo.oa@gmail.com>
2024-07-02 18:45:26 +0100

    change return type and remove args to app.run

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 
 """ import flask package """
+from typing import Callable
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def home() -> str:
+def home() -> Callable:
     """ render homepage """
     return render_template('0-index.html')
 
 
 if __name__ == "__main__":
-    app.run(port=5000, host="0.0.0.0")
+    app.run()
